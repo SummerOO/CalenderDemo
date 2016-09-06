cf10ca6aceb69ae76ba5435819855b6be29daa88
RuZhang <ru.zhang@newbeeair.com>
2016-09-06 16:55:30 +0800

    N/A

--- CalenderDemo/ViewController.swift
+++ CalenderDemo/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    
+    lazy var timeLabel: UILabel = {
+       let time = UILabel(frame: CGRectZero)
+        time.text = "xxxx-xx"
+        time.backgroundColor = UIColor.blackColor()
+        time.textColor = UIColor.whiteColor()
+        return time
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
